e1279045c844dcc2d6d21edf49c19c804d46b6dd
kbokulic <kbokulic@gmail.com>
2014-12-21 21:17:53 +0100

    Functions makeCacheMatrix and cacheSolve finished and tested

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions
+# makeCacheMatrix and cacheSolve create special datatype that knows how to create matrix and 
+# its inverse and how to boost performance by calculating inverse only once when matrix is 
+# created/changed
 
-## Write a short comment describing this function
+## makeCacheMatrix create special datatype of matrix that can save matrix and it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Matrix X inverse value inicialization
+  xInv <- NULL
+  
+  # Setters and getters for matrix and matrix inverse
+  set <- function (m){
+    x <<- m
+    xInv <<- NULL
+  }
+  
+  get <- function() return(x)
+  
+  setInv <- function(inv) xInv <<- inv
+  
+  getInv <- function() return(xInv)
+  
+  return(list(set=set, get=get, setInv=setInv, getInv=getInv))
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve return a matrix invere of 'x'.
+# Better performance becauase of matrix inverse chaching
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  xInv <- x$getInv()
+  if(!is.null(xInv)) {
+    message("Cached value")
+    return(x$getInv())
+  }
+  
+  # Else - calculate and set matrix inverse and return calculated value
+  x$setInv(solve(x$get()))
+  return(x$getInv())
+  
 }
+
+
+## Unit test
+mt1 <- makeCacheMatrix(matrix(1:4, nrow=2))
+mt1$get()
+
+cacheSolve(mt1)
+cacheSolve(mt1)
+# Setting new values
+mt1$set(matrix(2:5, nrow=2))
+cacheSolve(mt1)
+cacheSolve(mt1)
+# Unit test End
